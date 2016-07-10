d53fcb308a10b8a3b91bd171384e5d376f48794e
hrichas <hrichas@gmail.com>
2016-07-10 23:29:58 +0545

    topbar fixed and logo added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 $(document).foundation();
+$(document).ready(function(){
+   var scroll_start = 0;
+   var startchange = $('#top-bar');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() {
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > 460) {
+          $(".top-bar").css('background-color', '#f87373');
+       } else {
+          $('.top-bar').css('background-color', 'transparent');
+       }
+   });
+    }
+});
